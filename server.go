a07aa4d6e730895f057f0c59b52ea22741657908
Radon Rosborough <radon@intuitiveexplanations.com>
2024-02-19 16:06:33 -0800

    learn how to use concurrency

--- server.go
+++ server.go
@@ -15,119 +15,194 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func waitCondTimeout(c *sync.Cond, crit func() bool, timeout time.Duration) bool {
-	expired := false
-	time.AfterFunc(timeout, func() {
-		c.L.Lock()
-		expired = true
-		c.L.Unlock()
-		c.Broadcast()
-	})
-	c.L.Lock()
-	for crit() || expired {
-		c.Wait()
-	}
-	c.L.Unlock()
-	return expired
-}
+const (
+	Timing_RequestStaleness  = 15 * time.Second
+	Timing_DoorOpenDuration  = 10 * time.Second
+	Timing_DoorOpenCooldown  = 1 * time.Second
+	Timing_HeartbeatInterval = 1 * time.Second
+)
 
 type config struct {
-	DoorOpenSeconds    int    `env:"DOOR_OPEN_SECONDS,notEmpty"`
 	ControllerPassword string `env:"CONTROLLER_PASSWORD,notEmpty"`
 	RemotePassword     string `env:"REMOTE_PASSWORD,notEmpty"`
 	DisableTLS         bool   `env:"DISABLE_TLS"`
 }
 
-type server struct {
-	doorOpenSeconds int
+type syncvar[T any] struct {
+	Value T
+	Cond  *sync.Cond
+}
 
-	lock                  *sync.Mutex
-	controller            *controller
-	lastControllerCheckin *time.Time
-	doorLastOpened        *time.Time
+func NewSyncvar[T any]() syncvar[T] {
+	return syncvar[T]{
+		Cond: sync.NewCond(&sync.Mutex{}),
+	}
+}
+
+func (s syncvar[T]) Get() T {
+	s.Cond.L.Lock()
+	defer s.Cond.L.Unlock()
+	return s.Value
+}
+
+func (s syncvar[T]) Set(value T) {
+	s.Cond.L.Lock()
+	defer s.Cond.L.Unlock()
+	s.Value = value
+	s.Cond.Broadcast()
+}
+
+func (s syncvar[T]) WithValue(task func(T)) {
+	s.Cond.L.Lock()
+	defer s.Cond.L.Unlock()
+	task(s.Value)
+}
+
+func (s syncvar[T]) Update(xform func(T) T) {
+	s.Cond.L.Lock()
+	defer s.Cond.L.Unlock()
+	s.Value = xform(s.Value)
+	s.Cond.Broadcast()
+}
+
+func (s syncvar[T]) Wait(pred func(T) bool) {
+	s.WaitTimeout(pred, 0)
+}
+
+func (s syncvar[T]) WaitTimeout(pred func(T) bool, timeout time.Duration) error {
+	expired := false
+	if timeout > 0 {
+		time.AfterFunc(timeout, func() {
+			s.Cond.L.Lock()
+			defer s.Cond.L.Unlock()
+			expired = true
+			s.Cond.Broadcast()
+		})
+	}
+	s.Cond.L.Lock()
+	defer s.Cond.L.Unlock()
+	for pred(s.Value) || expired {
+		s.Cond.Wait()
+	}
+	if expired {
+		return errors.New("timed out")
+	}
+	return nil
+}
+
+type syncvarTimePtr struct {
+	syncvar[*time.Time]
+}
+
+func NewSyncvarTimePtr() syncvarTimePtr {
+	return syncvarTimePtr{NewSyncvar[*time.Time]()}
+}
+
+func (s syncvarTimePtr) UpdateToMaxWith(value time.Time) {
+	s.Update(func(existing *time.Time) *time.Time {
+		if existing == nil {
+			return &value
+		}
+		if value.After(*existing) {
+			return &value
+		}
+		return existing
+	})
+}
+
+type server struct {
+	activeController      syncvar[*controller]
+	lastControllerCheckin syncvarTimePtr
+	openRequestReceived   syncvarTimePtr
+	doorOpened            syncvarTimePtr
 }
 
 type controller struct {
-	lock sync.Mutex
+	server *server
 
 	conn   net.Conn
 	stream *bufio.ReadWriter
 
-	openRequestReceived *time.Time
-	openRequestCond     *sync.Cond
-
-	doorOpened   *time.Time
-	doorOpenCond *sync.Cond
-
-	isAlive   bool
-	aliveCond *sync.Cond
+	openAttemptMade syncvar[*time.Time]
 }
 
 func NewServer() *server {
 	return &server{
-		lock:                  &sync.Mutex{},
-		controller:            nil,
-		lastControllerCheckin: nil,
-		doorLastOpened:        nil,
+		activeController:      NewSyncvar[*controller](),
+		lastControllerCheckin: NewSyncvarTimePtr(),
+		openRequestReceived:   NewSyncvarTimePtr(),
+		doorOpened:            NewSyncvarTimePtr(),
 	}
 }
 
-func (s *server) NewController(conn net.Conn, stream *bufio.ReadWriter) *controller {
+func (s *server) RegisterController(conn net.Conn, stream *bufio.ReadWriter) {
 	c := controller{
-		conn:                conn,
-		stream:              stream,
-		openRequestReceived: nil,
-		openRequestCond:     sync.NewCond(&sync.Mutex{}),
-		doorOpened:          nil,
-		doorOpenCond:        sync.NewCond(&sync.Mutex{}),
-		isAlive:             true,
-		aliveCond:           sync.NewCond(&sync.Mutex{}),
+		server: s,
+		conn:   conn,
+		stream: stream,
 	}
-	go c.doReads(s)
-	go c.doCloses(s)
-	return &c
+	s.activeController.Set(&c)
+	go c.doReads()
+	go c.doWrites()
+	go c.doCloses()
 }
 
-func (s *server) Register(conn net.Conn, stream *bufio.ReadWriter) {
-	s.lock.Lock()
-	c := s.controller
-	c.aliveCond.L.Lock()
-	c.isAlive = false
-	c.aliveCond.Broadcast()
-	c.aliveCond.L.Unlock()
-	s.controller = s.NewController(conn, stream)
-	s.lock.Unlock()
+func (c *controller) IsRegistered() bool {
+	return c.server.activeController.Get() == c
 }
 
-func (s *server) Open() error {
-	s.lock.Lock()
-	if s.controller == nil {
-		s.lock.Unlock()
-		return errors.New("controller is offline")
-	}
-	if s.doorLastOpened.After(time.Now().Add(time.Duration(-s.doorOpenSeconds) * time.Second)) {
-		s.lock.Unlock()
-		return errors.New("door already opened")
-	}
-	c := s.controller
-	s.lock.Unlock()
-	return c.Open(s)
+func (c *controller) Deregister() {
+	c.server.activeController.Update(func(cc *controller) *controller {
+		if c == cc {
+			return nil
+		}
+		return cc
+	})
 }
 
-func (s *server) getController() *controller {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-	return s.controller
+func (c *controller) IfActive(task func()) {
+	c.server.activeController.WithValue(func(cc *controller) {
+		if c == cc {
+			task()
+		}
+	})
+}
+
+func (s *server) Open() (string, error) {
+	c := s.activeController.Get()
+	if c == nil {
+		return "", errors.New("no active controller")
+	}
+	now := time.Now()
+	s.openRequestReceived.UpdateToMaxWith(now)
+	var resp string
+	err := s.doorOpened.WaitTimeout(func(last *time.Time) bool {
+		if last == nil {
+			return false
+		}
+		if last.Before(now.Add(-Timing_DoorOpenDuration)) {
+			return false
+		}
+		if last.Before(now) {
+			resp = "door already opened"
+			return true
+		}
+		resp = "door opened"
+		return true
+	}, 5*time.Second)
+	if err != nil {
+		return "", fmt.Errorf("waiting for controller: %w", err)
+	}
+	return resp, nil
 }
 
 func (s *server) isHealthy() (bool, *time.Duration) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-	if s.lastControllerCheckin == nil {
+	last := s.lastControllerCheckin.Get()
+	if last == nil {
 		return false, nil
 	}
-	since := time.Now().Sub(*s.lastControllerCheckin)
-	return s.controller != nil && s.lastControllerCheckin.After(time.Now().Add(-60*time.Second)), &since
+	since := time.Now().Sub(*last)
+	return last.After(time.Now().Add(-60 * time.Second)), &since
 }
 
 func (s *server) isHealthyString() (bool, string) {
@@ -139,42 +214,12 @@ func (s *server) isHealthyString() (bool, string) {
 	return false, fmt.Sprintf("bad, no update yet\n")
 }
 
-func (s *server) recordCheckin(ts *time.Time, c *controller) {
-	s.lock.Lock()
-	if s.controller != c {
-		return
-	}
-	s.lastControllerCheckin = ts
-	s.lock.Unlock()
-}
-
-func (s *server) recordOpening(ts *time.Time, c *controller) {
-	s.lock.Lock()
-	if s.controller != c {
-		return
-	}
-	s.doorLastOpened = ts
-	s.lock.Unlock()
-}
-
-func (c *controller) Open(s *server) error {
-	c.openRequestCond.L.Lock()
-	now := time.Now()
-	c.openRequestReceived = &now
-	c.openRequestCond.Broadcast()
-	c.openRequestCond.L.Unlock()
-	expired := waitCondTimeout(c.doorOpenCond, func() bool {
-		return c.doorOpened != nil && c.doorOpened.After(time.Now().Add(time.Duration(-s.doorOpenSeconds)))
-	}, 3*time.Second)
-	if expired {
-		return errors.New("controller didn't respond in time")
-	}
-	return nil
-}
-
-func (c *controller) doReads(s *server) {
+func (c *controller) doReads() {
 	for {
-		c.conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		if !c.IsRegistered() {
+			return
+		}
+		c.conn.SetReadDeadline(time.Now().Add(3 * time.Second))
 		line, prefix, err := c.stream.ReadLine()
 		if err != nil {
 			log.Println("error reading line:", err)
@@ -186,33 +231,76 @@ func (c *controller) doReads(s *server) {
 		}
 		switch string(line) {
 		case "client ok":
-			now := time.Now()
-			s.recordCheckin(&now, c)
+			c.IfActive(func() {
+				c.server.lastControllerCheckin.UpdateToMaxWith(time.Now())
+			})
 		case "opened":
-			now := time.Now()
-			s.recordOpening(&now, c)
+			c.IfActive(func() {
+				c.server.doorOpened.UpdateToMaxWith(time.Now())
+			})
+		case "closed":
+			// nothing to do
+		default:
+			log.Println("unexpected line received")
 		}
 	}
 }
 
-func (c *controller) doWrites(s *server) {
+func (c *controller) shouldOpen(lastRequest *time.Time, lastAttempt *time.Time, lastSuccess *time.Time) bool {
+	// We have not been asked to open the door
+	if lastRequest == nil {
+		return false
+	}
+	// The request was from too long ago, something must have gone
+	// wrong, ignore it to avoid unexpected opens
+	if lastRequest.Before(time.Now().Add(-Timing_RequestStaleness)) {
+		return false
+	}
+	// We have already submitted an attempt for this request
+	if lastAttempt != nil && lastRequest.Before(*lastAttempt) {
+		return false
+	}
+	// The door is still open from last time
+	if lastSuccess != nil && lastRequest.Before(lastSuccess.Add(Timing_DoorOpenDuration+Timing_DoorOpenCooldown)) {
+		return false
+	}
+	return true
+}
+
+func (c *controller) doWrites() {
 	for {
-		lastPing := time.Unix(0, 0)
-		c.conn.SetWriteDeadline(time.Now().Add(5 * time.Second))
-		shouldOpen := !waitCondTimeout()
+		var shouldOpen bool
+		c.server.openRequestReceived.WaitTimeout(func(lastRequest *time.Time) bool {
+			lastAttempt := c.openAttemptMade.Get()
+			lastSuccess := c.server.doorOpened.Get()
+			shouldOpen = c.shouldOpen(lastRequest, lastAttempt, lastSuccess)
+			return shouldOpen
+		}, Timing_HeartbeatInterval)
+		if !c.IsRegistered() {
+			return
+		}
+		c.conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
+		if shouldOpen {
+			_, err := c.conn.Write([]byte("open sesame\n"))
+			if err != nil {
+				log.Println("error writing line:", err)
+				return
+			}
+		}
+		c.conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
+		_, err := c.conn.Write([]byte("server ok\n"))
+		if err != nil {
+			log.Println("error writing line:", err)
+			return
+		}
 	}
 }
 
-func (c *controller) doCloses(s *server) {
-	c.aliveCond.L.Lock()
-	for c.isAlive {
-		c.aliveCond.Wait()
-	}
-	c.aliveCond.L.Unlock()
+func (c *controller) doCloses() {
+	c.server.activeController.Wait(func(cc *controller) bool {
+		return c != cc
+	})
 	c.conn.Close()
-	s.lock.Lock()
-	s.controller = nil
-	s.lock.Unlock()
 }
 
 func mainE() error {
@@ -246,7 +334,7 @@ func mainE() error {
 			log.Println("unable to hijack http.ResponseWriter:", err)
 			w.WriteHeader(http.StatusInternalServerError)
 		}
-		s.Register(conn, stream)
+		s.RegisterController(conn, stream)
 	}).Methods("PUT")
 	r.HandleFunc("/api/v0/remote/status", func(w http.ResponseWriter, req *http.Request) {
 		if req.Header.Get("authorization") != fmt.Sprintf("MeLaan %s", cfg.RemotePassword) {
@@ -266,12 +354,13 @@ func mainE() error {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
-		err := s.Open()
+		resp, err := s.Open()
 		if err != nil {
 			w.WriteHeader(http.StatusBadGateway)
 			w.Write([]byte(err.Error() + "\n"))
 		}
 		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(resp + "\n"))
 	}).Methods("POST")
 	port, err := net.Listen("tcp", "0.0.0.0:8793")
 	if err != nil {
